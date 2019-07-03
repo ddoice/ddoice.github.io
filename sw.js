257bfb6c4cf5a715dd60f17ebc7a4a35e20021e3
rafael <rgarcia.cti.com@gmail.com>
2019-07-03 12:52:58 +0200

    update

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
 
 let counter = 0;
+let prev = 0;
 
 // The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', event => {
@@ -11,17 +12,35 @@ self.addEventListener('activate', event => {
 
   console.log('sw.js activate')
 
-  setInterval(()=>{
-    self.registration.showNotification(`Hi! ${counter++}`, {
-      tag: 'renotify',
-      renotify: true
-    })
+  setInterval(async () => {
+
+    caches.keys().then(function (names) {
+      for (let name of names)
+        caches.delete(name);
+    });
+
+    // const bg = self.registration.backgroundFetch.fetch(`my-fetch ${counter}`, ['/data.json'], {  title: 'json fake data'});
+    // const temp = await self.registration.backgroundFetch.get(`my-fetch ${counter}`);
+
+    const ts = Math.floor(Date.now() / 1000);
+    const data = await fetch(`./data.json?${ts}`).then(res=>res.json())
+
+    if(prev!== 0 && prev !== data.length) {
+      self.registration.showNotification(`Changed! (${counter++}) - json.length: ${data.length} - ${ts}`, {
+        tag: 'renotify',
+        renotify: true
+      });
+    }
+
+    prev = data.length;
+
+    //console.log('data', bg, temp)
+    //console.log('self.backgroundFetch', self.registration.backgroundFetch)
+    
   }, 5000)
 
 });
 
-fetch('/data.json').then(r=>r.json()).then(data=>{
-  console.log('sw.js data=', data);
-});
+
 
 
